4431146d8ae77d0f7b6730e32094ff48f1025404
Miguel de Icaza <miguel@gnome.org>
2010-10-22 23:02:05 -0400

    Add Security framework

---
+++ src/Security/Certificate.cs
@@ -0,0 +1,116 @@
+// 
+// Certificate.cs: Implements the managed SecCertificate wrapper.
+//
+// Authors: Miguel de Icaza
+//     
+// Copyright 2010 Novell, Inc
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+using System;
+using MonoMac.ObjCRuntime;
+using MonoMac.CoreFoundation;
+
+namespace MonoMac.Security {
+
+	public class SecCertificate : INativeObject, IDisposable {
+		internal IntPtr handle;
+		
+		// invoked by marshallers
+		internal SecCertificate (IntPtr handle)
+			: this (handle, false)
+		{
+		}
+		
+		[Preserve (Conditional = true)]
+		internal SecCertificate (IntPtr handle, bool owns)
+		{
+			this.handle = handle;
+			if (!owns)
+				CFObject.CFRetain (handle);
+		}
+
+		[DllImport (Constants.SecurityLibrary)]
+		extern static IntPtr SecCertificateCreateWithData (IntPtr allocator, IntPtr cfData);
+
+		public SecCertificate (NSData data)
+		{
+			if (data == null)
+				throw new ArgumentNullException ("data");
+
+			handle = SecCertificateCreateWithData (IntPtr.Zero, data.Handle);
+			if (handle == IntPtr.Zero)
+				throw new ArgumentException ("Not a valid DER-encoded X.509 certificate");
+		}
+
+		[DllImport (Constants.SecurityLibrary)]
+		extern static IntPtr SecCertificateCopySubjectSummary (IntPtr cert);
+
+		public string SubjectSummary {
+			get {
+				if (handle == IntPtr.Zero)
+					throw new ObjectDisposedException ();
+				
+				IntPtr cfstr = SecCertificateCopySubjectSummary (handle);
+				string ret = CFString.FetchString (cfstring);
+				CFObject.CFRelease (cfstr);
+				return ret;
+			}
+		}
+
+		public NSData DerData {
+			get {
+				if (handle == IntPtr.Zero)
+					throw new ObjectDisposedException ();
+
+				IntPtr data = SecCertificateCopyData (handle);
+				if (data == null)
+					throw new ArgumentException ("Not a valid certificate");
+				return new NSData (data);
+			}
+		}
+		
+		~SecCertificate ()
+		{
+			Dispose (false);
+		}
+
+		public IntPtr Handle {
+			get {
+				return handle;
+			}
+		}
+
+		public void Dispose ()
+		{
+			Dispose (true);
+			GC.SuppressFinalize (this);
+		}
+
+		public virtual void Dispose (bool disposing)
+		{
+			if (handle != IntPtr.Zero){
+				CFObject.CFRelease (handle);
+				handle = IntPtr.Zero;
+			}
+		}
+		
+	}
+}
---
+++ src/Security/Enums.cs
@@ -0,0 +1,15 @@
+
+namespace MonoMac.Security {
+
+	public enum SecStatusCode {
+		Success = 0,
+		Unimplemented = -4,
+		Param = -50,
+		Allocate = -108,
+		NotAvailable = -25291,
+		DuplicateItem = -25299,
+		ItemNotFound = -25300,
+		InteractionNotAllowed = -25308,
+		Decode = -26275, 
+	}
+}
