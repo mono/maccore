b37c40d9869d7b599a54b63b66c242837e627ca9
Geoff Norton <grompf@sublimeintervention.com>
2010-11-30 12:40:15 -0500

    Remove erroneous setter

--- src/coreanimation.cs
+++ src/coreanimation.cs
@@ -458,7 +458,7 @@ namespace MonoMac.CoreAnimation {
 	[BaseType (typeof (CALayer))]
 	interface CATiledLayer {
 		[Static][Export ("fadeDuration")]
-		double FadeDuration { get; set; }
+		double FadeDuration { get; ]
 
 		[Export ("levelsOfDetail")]
 		int LevelsOfDetail { get; set; }
