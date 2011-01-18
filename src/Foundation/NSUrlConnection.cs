5419b461450cda52197197034c1286695bf86939
Geoff Norton <grompf@sublimeintervention.com>
2011-01-18 17:56:17 -0500

    Fix NSUrlConnection:SendSynchronousRequest binding

--- src/Foundation/NSUrlConnection.cs
+++ src/Foundation/NSUrlConnection.cs
@@ -16,24 +16,32 @@ namespace MonoMac.Foundation {
 	public partial class NSUrlConnection {
                 static Selector selSendSynchronousRequestReturningResponseError = new Selector ("sendSynchronousRequest:returningResponse:error:");
 		
-		unsafe static NSData SendSynchronousRequest (NSUrlRequest request, out NSUrlResponse response, NSError error)
+		unsafe static NSData SendSynchronousRequest (NSUrlRequest request, out NSUrlResponse response, out NSError error)
 		{
-			IntPtr storage = IntPtr.Zero;
+			IntPtr responseStorage = IntPtr.Zero;
+			IntPtr errorStorage = IntPtr.Zero;
 
-			void *p = &storage;
-			IntPtr handle = (IntPtr) p;
+			void *resp = &responseStorage;
+			void *errp = &errorStorage;
+			IntPtr rhandle = (IntPtr) resp;
+			IntPtr ehandle = (IntPtr) errp;
 			
 			var res = Messaging.IntPtr_objc_msgSend_IntPtr_IntPtr_IntPtr (
 				class_ptr,
 				selSendSynchronousRequestReturningResponseError.Handle,
 				request.Handle,
-				handle,
-				error != null ? error.Handle : IntPtr.Zero);
+				rhandle,
+				ehandle);
 
-			if (storage != IntPtr.Zero)
-				response = (NSUrlResponse) Runtime.GetNSObject (storage);
+			if (responseStorage != IntPtr.Zero)
+				response = (NSUrlResponse) Runtime.GetNSObject (responseStorage);
 			else
 				response = null;
+
+			if (errorStorage != IntPtr.Zero)
+				error = (NSUrlResponse) Runtime.GetNSObject (errorStorage);
+			else
+				error = null;
 			
 			return (NSData) Runtime.GetNSObject (res);
 		}
