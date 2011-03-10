mono/maccore

commit ffc4514663d3f429b9ecf14bb7ead9c085a264e2

Fix by Kenneth

src/foundation.cs
@@ -1349,7 +1349,7 @@ namespace MonoMac.Foundation
 		[Field ("NSModalPanelRunLoopMode", "AppKit")]
 		NSString NSRunLoopModalPanelMode { get; }
 
-		[Field ("NSEventTrackingRunLoopMode")]
+		[Field ("NSEventTrackingRunLoopMode"), "AppKit"]
 		NSString NSRunLoopEventTracking { get; }
 #endif
 	}

commit 836aa4b93a91cdb831285691bd26c38d408ad582

Fix a build break introduced in ffc4514663d3f429b9ecf14bb7ead9c085a264e2

src/foundation.cs
@@ -1349,7 +1349,7 @@ namespace MonoMac.Foundation
 		[Field ("NSModalPanelRunLoopMode", "AppKit")]
 		NSString NSRunLoopModalPanelMode { get; }
 
-		[Field ("NSEventTrackingRunLoopMode"), "AppKit"]
+		[Field ("NSEventTrackingRunLoopMode", "AppKit")]
 		NSString NSRunLoopEventTracking { get; }
 #endif
 	}