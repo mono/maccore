a3d3939d46c02d6d2d0421d5cceccfa8ef5b2050
Miguel de Icaza <miguel@gnome.org>
2011-09-28 22:49:23 -0400

    Allow to construct AudioSource objects without being fully initialized, to allow users to plug their own code

--- src/AudioToolbox/AudioFile.cs
+++ src/AudioToolbox/AudioFile.cs
@@ -1056,6 +1056,15 @@ namespace MonoMac.AudioToolbox {
 
 		public AudioSource (AudioFileType inFileType, AudioStreamBasicDescription format) : base (true)
 		{
+			Initialize (inFileType, format);
+		}
+
+		public AudioSource () : base (true)
+		{
+		}
+			
+		protected void Initialize (AudioFileType inFileType, AudioStreamBasicDescription format) 
+		{
 			IntPtr h;
 
 			gch = GCHandle.Alloc (this);
