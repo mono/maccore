3415f7c59d52b468f5e6315aa766a3e6790b0b27
Jeffrey Stedfast <fejj@gnome.org>
2011-10-03 15:36:44 -0700

    Merge pull request #18 from jacksonh/patch-2
    Allow AudioSource's to be opened with callbacks outside of the ctor.

--- src/AudioToolbox/AudioFile.cs
+++ src/AudioToolbox/AudioFile.cs
@@ -1103,6 +1103,11 @@ namespace MonoMac.AudioToolbox {
 		
 		public AudioSource (AudioFileType fileTypeHint) : base (true)
 		{
+			Open (fileTypeHint);
+		}
+		
+		protected void Open (AudioFileType fileTypeHint)
+		{
 			IntPtr h;
 
 			gch = GCHandle.Alloc (this);
