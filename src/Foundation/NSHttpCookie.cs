b5e8d21f68909cfeb6c54a65092cd3bc17cf80f4
Sebastien Pouliot <sebastien@xamarin.com>
2012-02-23 11:07:50 -0500

    Add new constructors to ease creating NSHttpCookie without a NSDictionary and with a System.Net.Cookie

--- src/Foundation/NSHttpCookie.cs
+++ src/Foundation/NSHttpCookie.cs
@@ -1,5 +1,6 @@
 using MonoMac.ObjCRuntime;
 using System;
+using System.Net;
 
 namespace MonoMac.Foundation {
 	public partial class NSHttpCookie {
@@ -39,6 +40,80 @@ namespace MonoMac.Foundation {
 				KeyPort = Dlfcn.GetStringConstant (handle, "NSHTTPCookiePort");
 			} finally {
 				Dlfcn.dlclose (handle);
+			}
+		}
+		
+		// same order as System.Net.Cookie
+		// http://msdn.microsoft.com/en-us/library/a18ka3h2.aspx
+		public NSHttpCookie (string name, string value) : this (name, value, null, null)
+		{
+			CreateCookie (name, value, null, null, null, null, null, null, null, null, null, null);
+		}
+		
+		public NSHttpCookie (string name, string value, string path) : this (name, value, path, null)
+		{
+			CreateCookie (name, value, path, null, null, null, null, null, null, null, null, null);
+		}
+
+		public NSHttpCookie (string name, string value, string path, string domain)
+		{
+			CreateCookie (name, value, path, domain, null, null, null, null, null, null, null, null);
+		}
+		
+		// FIXME: should we expose more complex/long ctor or point people to use a Cookie ?
+
+		public NSHttpCookie (Cookie cookie)
+		{
+			if (cookie == null)
+				throw new ArgumentNullException ("cookie");
+			
+			string commentUrl = cookie.CommentUri != null ? cookie.CommentUri.ToString () : null;
+			CreateCookie (cookie.Name, cookie.Value, cookie.Path, cookie.Domain, cookie.Comment, commentUrl, cookie.Discard, cookie.Expires, null, cookie.Port, cookie.Secure, cookie.Version);
+		}
+		
+		void CreateCookie (string name, string value, string path, string domain, string comment, string commentUrl, bool? discard, DateTime? expires, int? maximumAge, string ports, bool? secure, int? version)
+		{
+			// mandatory checks or defaults
+			if (name == null)
+				throw new ArgumentNullException ("name");
+			if (value == null)
+				throw new ArgumentNullException ("value");
+			if (String.IsNullOrEmpty (path))
+				path = "/"; // default in .net
+			if (String.IsNullOrEmpty (domain))
+				domain = "*";
+
+			using (var properties = new NSMutableDictionary ()) {
+				// mandatory to create the cookie
+				properties.Add (NSHttpCookie.KeyName, new NSString (name));
+				properties.Add (NSHttpCookie.KeyValue, new NSString (value));
+				properties.Add (NSHttpCookie.KeyPath, new NSString (path));
+				properties.Add (NSHttpCookie.KeyDomain, new NSString (domain));
+				
+				// optional to create the cookie
+				if (!String.IsNullOrEmpty (comment))
+					properties.Add (NSHttpCookie.KeyComment, new NSString (comment));
+				if (!String.IsNullOrEmpty (commentUrl))
+					properties.Add (NSHttpCookie.KeyCommentURL, new NSString (commentUrl));
+				if (discard.HasValue)
+					properties.Add (NSHttpCookie.KeyDiscard, new NSString (discard.Value ? "TRUE" : "FALSE"));
+				if (expires.HasValue && expires.Value != DateTime.MinValue)
+					properties.Add (NSHttpCookie.KeyExpires, (NSDate) expires.Value);
+				if (maximumAge.HasValue)
+					properties.Add (NSHttpCookie.KeyMaximumAge, new NSString (maximumAge.Value.ToString ()));
+				if (ports != null)
+					properties.Add (NSHttpCookie.KeyPort, new NSString (ports.Replace ("\"", String.Empty)));
+				// any value means secure is true
+				if (secure.HasValue && secure.Value)
+					properties.Add (NSHttpCookie.KeySecure, new NSString ("1"));
+				if (version.HasValue)
+					properties.Add (NSHttpCookie.KeyVersion, new NSString (version.Value.ToString ()));
+				
+				if (IsDirectBinding) {
+					Handle = MonoTouch.ObjCRuntime.Messaging.IntPtr_objc_msgSend_IntPtr (this.Handle, selInitWithProperties_, properties.Handle);
+				} else {
+					Handle = MonoTouch.ObjCRuntime.Messaging.IntPtr_objc_msgSendSuper_IntPtr (this.SuperHandle, selInitWithProperties_, properties.Handle);
+				}
 			}
 		}
 	}
