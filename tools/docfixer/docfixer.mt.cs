5e88841ffd9f4db27d85da090650d60eb667d044
Jeremie Laval <jeremie.laval@gmail.com>
2012-04-04 16:31:17 +0100

    [docfixer] Backport changes from AppleDocWizard

--- tools/docfixer/docfixer.mt.cs
+++ tools/docfixer/docfixer.mt.cs
@@ -4,16 +4,13 @@
 using System;
 using System.IO;
 using System.Reflection;
-using MonoTouch.Foundation;
 
-public partial class DocGenerator {
-	static Assembly assembly = typeof (MonoTouch.Foundation.NSObject).Assembly;
+public partial class DocGenerator
+{
 	const string BaseNamespace = "MonoTouch";
 
 	static string GetMostRecentDocBase ()
 	{
-		//var versions = new[]{"4_0", "3_2", "3_1"};
-		//string format = "/Developer/Platforms/iPhoneOS.platform/Developer/Documentation/DocSets/com.apple.adc.documentation.AppleiPhone{0}.iPhoneLibrary.docset/Contents/Resources/Documents/documentation";
 		var versions = new [] { "5_0" };
 		var base_paths = new [] {
 			"/Library/Developer/Shared/Documentation/DocSets/com.apple.adc.documentation.AppleiOS{0}.iOSLibrary.docset/Contents/Resources/Documents/documentation",
@@ -30,10 +27,5 @@ public partial class DocGenerator {
 			}
 		}
 		return null;
-	}
-
-	public static string GetSelector (object attr)
-	{
-		return ((ExportAttribute) attr).Selector;
 	}
 }
