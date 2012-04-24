c3932dad8fa4b5268560a5280024c806fae4c4f2
Miguel de Icaza <miguel@gnome.org>
2012-04-24 00:17:06 -0400

    Fix namespaces to share the code with MonoMac

--- src/AVFoundation/AVCaptureConnection.cs
+++ src/AVFoundation/AVCaptureConnection.cs
@@ -25,9 +25,9 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-using MonoTouch.ObjCRuntime;
+using MonoMac.ObjCRuntime;
 
-namespace MonoTouch.AVFoundation {
+namespace MonoMac.AVFoundation {
 	public partial class AVCaptureConnection {
 
 		public bool SupportsVideoMinFrameDuration {
