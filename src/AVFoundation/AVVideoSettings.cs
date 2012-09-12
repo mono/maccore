064a51a830120803ee21ebc7d980d3163dadc1ea
Marek Safar <marek.safar@gmail.com>
2012-09-12 09:46:52 +0100

    Implement nested codec settings

--- src/AVFoundation/AVVideoSettings.cs
+++ src/AVFoundation/AVVideoSettings.cs
@@ -48,6 +48,19 @@ namespace MonoMac.AVFoundation {
 		ResizeAspectFill
 	}
 
+	public enum AVVideoProfileLevelH264
+	{
+		Baseline30 = 1,
+		Baseline31,
+		Baseline41,
+ 		Main30,
+		Main31,
+		Main32,
+		Main41,
+		High40,
+		High41
+	}
+
 	public class AVVideoSettingsUncompressed : PixelBufferAttributes
 	{
 #if !COREBUILD
@@ -176,7 +189,195 @@ namespace MonoMac.AVFoundation {
 					SetNativeValue (AVVideo.ScalingModeKey, v);
 			}
 		}
+
+		public AVVideoCodecSettings CodecSettings {
+			set {
+				SetNativeValue (AVVideo.CompressionPropertiesKey, value == null ? null : value.Dictionary);
+			}
+		}
+#endif
+	}
+
+	public class AVVideoCodecSettings : DictionaryContainer
+	{
+#if !COREBUILD
+		public AVVideoCodecSettings ()
+			: base (new NSMutableDictionary ())
+		{
+		}
+
+		public AVVideoCodecSettings (NSDictionary dictionary)
+			: base (dictionary)
+		{
+		}
+
+		public int? AverageBitRate {
+			set {
+				SetNumberValue (AVVideo.AverageBitRateKey, (int?) value);
+			}
+			get {
+				return GetInt32Value (AVVideo.AverageBitRateKey);
+			}
+		}
+
+		public float? JPEGQuality {
+			set {
+				SetNumberValue (AVVideo.QualityKey, value);
+			}
+			get {
+				return GetFloatValue (AVVideo.QualityKey);
+			}
+		}
+
+		public int? MaxKeyFrameInterval {
+			set {
+				SetNumberValue (AVVideo.MaxKeyFrameIntervalKey, (int?) value);
+			}
+			get {
+				return GetInt32Value (AVVideo.MaxKeyFrameIntervalKey);
+			}
+		}
+
+		public AVVideoProfileLevelH264? ProfileLevelH264 {
+			set {
+				NSString v;
+				switch (value) {
+				case AVVideoProfileLevelH264.Baseline30:
+					v = AVVideo.ProfileLevelH264Baseline30;
+					break;
+				case AVVideoProfileLevelH264.Baseline31:
+					v = AVVideo.ProfileLevelH264Baseline31;
+					break;
+				case AVVideoProfileLevelH264.Baseline41:
+					v = AVVideo.ProfileLevelH264Baseline41;
+					break;
+				case AVVideoProfileLevelH264.Main30:
+					v = AVVideo.ProfileLevelH264Main30;
+					break;
+				case AVVideoProfileLevelH264.Main31:
+					v = AVVideo.ProfileLevelH264Main31;
+					break;
+				case AVVideoProfileLevelH264.Main32:
+					v = AVVideo.ProfileLevelH264Main32;
+					break;
+				case AVVideoProfileLevelH264.Main41:
+					v = AVVideo.ProfileLevelH264Main41;
+					break;
+				case AVVideoProfileLevelH264.High40:
+					v = AVVideo.ProfileLevelH264High40;
+					break;
+				case AVVideoProfileLevelH264.High41:
+					v = AVVideo.ProfileLevelH264High41;
+					break;
+				case null:
+					v = null;
+					break;
+				default:
+					throw new ArgumentException ("value");
+				}
+
+				if (v == null)
+					RemoveValue (AVVideo.ProfileLevelKey);
+				else
+					SetNativeValue (AVVideo.ProfileLevelKey, v);
+			}
+		}
+
+		public AVVideoPixelAspectRatioSettings PixelAspectRatio {
+			set {
+				SetNativeValue (AVVideo.PixelAspectRatioKey, value == null ? null : value.Dictionary);
+			}
+		}
+
+		public AVVideoCleanApertureSettings VideoCleanAperture {
+			set {
+				SetNativeValue (AVVideo.CleanApertureKey, value == null ? null : value.Dictionary);
+			}			
+		} 
+#endif
+	}
+
+	public class AVVideoPixelAspectRatioSettings : DictionaryContainer
+	{
+#if !COREBUILD
+		public AVVideoPixelAspectRatioSettings ()
+			: base (new NSMutableDictionary ())
+		{
+		}
+
+		public AVVideoPixelAspectRatioSettings (NSDictionary dictionary)
+			: base (dictionary)
+		{
+		}
+
+		public int? HorizontalSpacing {
+			set {
+				SetNumberValue (AVVideo.PixelAspectRatioHorizontalSpacingKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.PixelAspectRatioHorizontalSpacingKey);
+			}
+		}
+
+		public int? VerticalSpacing {
+			set {
+				SetNumberValue (AVVideo.PixelAspectRatioVerticalSpacingKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.PixelAspectRatioVerticalSpacingKey);
+			}
+		}
+#endif
+	}
+
+	public class AVVideoCleanApertureSettings : DictionaryContainer
+	{
+#if !COREBUILD
+		public AVVideoCleanApertureSettings ()
+			: base (new NSMutableDictionary ())
+		{
+		}
+
+		public AVVideoCleanApertureSettings (NSDictionary dictionary)
+			: base (dictionary)
+		{
+		}
+
+		public int? Width {
+			set {
+				SetNumberValue (AVVideo.CleanApertureWidthKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.CleanApertureWidthKey);
+			}
+		}
+
+		public int? Height {
+			set {
+				SetNumberValue (AVVideo.CleanApertureHeightKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.CleanApertureHeightKey);
+			}
+		}
+
+		public int? HorizontalOffset {
+			set {
+				SetNumberValue (AVVideo.CleanApertureHorizontalOffsetKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.CleanApertureHorizontalOffsetKey);
+			}
+		}
+
+		public int? VerticalOffset {
+			set {
+				SetNumberValue (AVVideo.CleanApertureVerticalOffsetKey, value);
+			}
+			get {
+				return GetInt32Value (AVVideo.CleanApertureVerticalOffsetKey);
+			}
+		}
 #endif
 	}
 }
-
