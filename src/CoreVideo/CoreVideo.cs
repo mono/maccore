3e85098e1b4ea7f068e23c00e56f385d63bb0ace
Marek Safar <marek.safar@gmail.com>
2012-09-13 08:22:06 +0100

    Add missing error code

--- src/CoreVideo/CoreVideo.cs
+++ src/CoreVideo/CoreVideo.cs
@@ -60,7 +60,7 @@ namespace MonoMac.CoreVideo {
 	public enum CVReturn {
 		Success = 0,
 		First = -6660,
-		Error = -6660,
+		Error = First,
 		InvalidArgument = -6661,
 		AllocationFailed = -6662,
 		InvalidDisplay = -6670,
@@ -71,6 +71,7 @@ namespace MonoMac.CoreVideo {
 		InvalidSize = -6681,
 		InvalidPixelBufferAttributes = -6682,
 		PixelBufferNotOpenGLCompatible = -6683,
+		WouldExceedAllocationThreshold = -6689,
 		PoolAllocationFailed = -6690,
 		InvalidPoolAttributes = -6691,
 		Last = -6699,
