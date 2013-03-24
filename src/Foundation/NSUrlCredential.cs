7a4a8bab4e9577aebfc934137f2f28677396803b
Sebastien Pouliot <sebastien@xamarin.com>
2013-03-24 13:03:41 -0400

    Fix MonoMac/MonoTouch (for MonoMac builds)

--- src/Foundation/NSUrlCredential.cs
+++ src/Foundation/NSUrlCredential.cs
@@ -14,9 +14,9 @@ namespace MonoMac.Foundation {
 		public NSUrlCredential (IntPtr trust, bool ignored) : base (NSObjectFlag.Empty)
 		{
 			if (IsDirectBinding) {
-				Handle = MonoTouch.ObjCRuntime.Messaging.IntPtr_objc_msgSend_IntPtr (this.Handle, Selector.GetHandle ("initWithTrust:"), trust);
+				Handle = Messaging.IntPtr_objc_msgSend_IntPtr (this.Handle, Selector.GetHandle ("initWithTrust:"), trust);
 			} else {
-				Handle = MonoTouch.ObjCRuntime.Messaging.IntPtr_objc_msgSendSuper_IntPtr (this.SuperHandle, Selector.GetHandle ("initWithTrust:"), trust);
+				Handle = Messaging.IntPtr_objc_msgSendSuper_IntPtr (this.SuperHandle, Selector.GetHandle ("initWithTrust:"), trust);
 			}
 		}
 	}
